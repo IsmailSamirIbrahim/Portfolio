e4e348c64aa3cf6711281ef1be4923a6c8fd0b3f
IsmailSamirIbrahim <ismail.samir.ibrahim@gmail.com>
2024-03-04 16:28:36 +0200

    Define the style for the collapsibles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+    item.addEventListener("click", function () {
+    document.querySelector(".fa").classList.toggle("fa-bars");
+    document.querySelector(".fa").classList.toggle("fa-close");
+    this.classList.toggle("collapsible--expanded");
+})
+);
