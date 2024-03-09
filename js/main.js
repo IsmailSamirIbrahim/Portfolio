718c7cf8c102630fcf68cf266afb6a15a284cec0
IsmailSamirIbrahim <ismail.samir.ibrahim@gmail.com>
2024-03-09 13:34:38 +0200

    Define initial style for the navigation bar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
     item.addEventListener("click", function () {
-    document.querySelector(".fa").classList.toggle("fa-bars");
-    document.querySelector(".fa").classList.toggle("fa-close");
     this.classList.toggle("collapsible--expanded");
 })
 );
